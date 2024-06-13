95894af9d1a2cc1269523d6f181d1a44a0e73d28
Malte Teichert <malte@malteteichert.de>
2024-06-14 00:45:07 +0200

    initial commit

---
+++ src/app.d.ts
@@ -0,0 +1,13 @@
+import type { AvailableLanguageTag } from "../../lib/paraglide/runtime"
+import type { ParaglideLocals } from "@inlang/paraglide-sveltekit"
+// See https://kit.svelte.dev/docs/types#app
+// for information about these interfaces
+// and what to do when importing types
+declare namespace App {
+	 interface Locals {
+    paraglide: ParaglideLocals<AvailableLanguageTag>,
+}
+	// interface PageData {}
+	// interface Error {}
+	// interface Platform {}
+}
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit'
+	import { i18n } from '$lib/i18n'
+
+	import '../app.postcss';
+</script>
+<ParaglideJS {i18n}>
+
+
+<slot />
+
+</ParaglideJS>
