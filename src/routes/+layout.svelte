5aae3f5fc222c8a0f44db024c8b82a57ca58186a
Malte Teichert <malte@malteteichert.de>
2024-06-14 02:08:20 +0200

    format code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
-	import { ParaglideJS } from '@inlang/paraglide-sveltekit'
-	import { i18n } from '$lib/i18n'
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	import { i18n } from '$lib/i18n';
 
 	import '../app.postcss';
 </script>
+
 <ParaglideJS {i18n}>
-
-
-<slot />
-
+	<slot />
 </ParaglideJS>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { paraglide } from '@inlang/paraglide-sveltekit/vite'
+import { paraglide } from '@inlang/paraglide-sveltekit/vite';
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [paraglide({ project: './project.inlang', outdir: './src/lib/paraglide' }),sveltekit(), purgeCss()]
+	plugins: [
+		paraglide({ project: './project.inlang', outdir: './src/lib/paraglide' }),
+		sveltekit(),
+		purgeCss()
+	]
 });
